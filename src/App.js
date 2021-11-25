c2512f10450947f3e3f5c9f5fe9776cbeb6c59a0
Subashree <77397550+suba2703@users.noreply.github.com>
2021-11-25 16:35:38 +0530

    Changes Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
+import QuizQuestion   from './QuizQuestion'
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <QuizQuestion />
+   
     )
   }
 }
